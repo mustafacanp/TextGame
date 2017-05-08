e984dc2e8638381cd2616e88606d106bc8e2cb67
Mustafa Can Palaz <esogu93@gmail.com>
2017-05-08 20:36:12 +0300

    responsive finished and mozilla overflow fixed

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 var start = function () {
 
     var pressEnter = function(){ // Enter'a basınca newLine() fonksiyonu ile yeni satıra geçme
-        $(document).keypress(function(e) {
+        $(document).on("keypress",function(e) {
             if(e.which == 13) {
                 newLine();
             }
@@ -116,8 +116,8 @@ var start = function () {
     return {
         init: function () { // Sayfa yüklenince (initialize olunca)
             //createDialogue("name_dialogue");
-            cout("", "Welcome visitor. Press enter for continue...<br><br>", "green", 0);
-            //refreshInputLine(); // İlk satırı oluşturdu.
+            cout("", "Welcome visitor. Press enter to continue...<br><br>", "green", 0);
+            refreshInputLine(); // İlk satırı oluşturdu.
             focusInput(); // Sayfada herhangi bir yere basınca input alanına focus olur.
             pressEnter(); // Enter kontrolünü ekledi.
         }
@@ -183,3 +183,46 @@ $("#statistics-button").click(function(){
     }
 });
 
+
+
+/*
+var canvas = document.getElementById("matrix");
+var ctx = canvas.getContext("2d");
+canvas.height = window.innerHeight;
+canvas.width = window.innerWidth;
+var matrix = "ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789@#$%^&*()*&^%";
+matrix = matrix.split("");
+var font_size = 10;
+//an array of drops - one per column
+var drops = [];
+//x below is the x coordinate
+//1 = y co-ordinate of the drop(same for every drop initially)
+for(var x = 0; x < columns; x++)
+    drops[x] = 1; 
+
+//drawing the characters
+function drawMatrix(){
+    ctx.fillStyle = "rgba(0, 0, 0, 0.04)";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    ctx.fillStyle = "#0F0"; //green text
+    ctx.font = font_size + "px arial";
+    //looping over drops
+    for(var i = 0; i < drops.length; i++)
+    {
+        //a random chinese character to print
+        var text = matrix[Math.floor(Math.random()*matrix.length)];
+        //x = i*font_size, y = value of drops[i]*font_size
+        ctx.fillText(text, i*font_size, drops[i]*font_size);
+
+        //sending the drop back to the top randomly after it has crossed the screen
+        //adding a randomness to the reset to make the drops scattered on the Y axis
+        if(drops[i]*font_size > canvas.height && Math.random() > 0.975)
+            drops[i] = 0;
+
+        //incrementing Y coordinate
+        drops[i]++;
+    }
+}
+setInterval(drawMatrix, 35);
+*/
